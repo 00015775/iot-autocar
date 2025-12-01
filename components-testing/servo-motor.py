3455af5ec2675c9e0d3469557698a755109632cf
00015775 <00015775WIUT@gmail.com>
2025-12-01 15:27:41 +0500

    Tests Servo Motor SG90 - rotates from 0 to 180 degree - positional rotation

--- components-testing/servo-motor.py
+++ components-testing/servo-motor.py
@@ -1,1 +1,42 @@
-# testing motor
+# testing servo motor
+
+from gpiozero import Servo
+from time import sleep
+ 
+myGPIO=19 # PWM enabled GPIO
+ 
+servo = Servo(myGPIO)
+ 
+while True:
+    servo.mid()
+    print("90 degree")
+    sleep(0.5)
+    servo.min()
+    print("0 degree")
+    sleep(1)
+    servo.mid()
+    print("90 degree")
+    sleep(0.5)
+    servo.max()
+    print("180 degree")
+    sleep(1)
+
+
+"""
+servo.value = [-1: 0 degree, 
+               0: 90 degree,
+               1: 180 degree]
+
+Pins:
+YELLOW - VCC, 5V
+RED - Any GPIO pins, but use PWN enabled pins (BCM: 12,13,18,19) if any left.
+BROWN - GND
+
+Referece:
+Code adapted from https://www.raspberrypi-spy.co.uk/2018/02/basic-servo-use-with-the-raspberry-pi/
+Information learned from https://docs.arduino.cc/tutorials/generic/basic-servo-control/
+
+If using external power supply for servo motor then read (both Power Supply and RPI/Arduino must have the COMMON GND): 
+https://docs.arduino.cc/learn/electronics/servo-motors/
+
+"""
